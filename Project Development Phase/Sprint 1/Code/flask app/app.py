326169e9acde11c9519757f662552f5bf15c5c42
dhivagar005 <112692979+dhivagar005@users.noreply.github.com>
2022-11-18 22:22:01 +0530

    Update app.py

--- Project Development Phase/Sprint 1/Code/flask app/app.py
+++ Project Development Phase/Sprint 1/Code/flask app/app.py
@@ -1,183 +1,103 @@
-from flask import Flask,render_template,redirect
-from flask import url_for,session,request
-from dotenv import load_dotenv
+from flask import Flask,render_template,request,url_for,redirect
+import ibm_db
+import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-import os
-import hashlib
-import re
-import ibm_db
-load_dotenv()
-
-app = Flask(__name__)
-app.secret_key = os.urandom(16)
-
-try:
-    conn = ibm_db.connect(os.getenv('CREDENTIALS'),'','')
-except Exception as err:
-     print("Exception occurs->", err)
+import requests
 
 
 
-@app.route('/')
-def index():
-    if not session or not session['login_status']:
-        return render_template('index.htm')
+app=Flask(__name__)
 
-    return redirect(url_for('home'))
-
-
-
-@app.route('/login')
-def login():
-    if not session or not session['login_status']:
-        return render_template('login.htm')
-
-    return redirect(url_for('home'))
-
-
-@app.route('/register')
-def register():
-    if not session or not session['login_status']:
-        return render_template('register.htm')
-
-    return redirect(url_for('home'))
-
-
-
-
-@app.route('/home')
+@app.route("/")
 def home():
-    if not session:
-        return redirect(url_for('login'))
+    return render_template('Home.html')
 
-    if session['login_status']:
-        return render_template('home.htm',username=session['user_id'])
+@app.route("/reg")
+def reg():
+    return render_template('Registration.html')
 
-    return redirect(url_for('login'))
-
-
-
-
-
-
-
-@app.route('/do_register',methods=['GET','POST'])
-def do_register():
-    if request.method == 'POST':
-        first_name = request.form['fname']
-        last_name = request.form['lname']
+@app.route("/register",methods=["POST","GET"])
+def register():
+     if request.method == 'POST' :
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        global email
         email = request.form['email']
-        addrss1 = request.form['Locality']
-        addrss2 = request.form['address']
-        state = request.form['State']
-        pincode = request.form['Zip']
-        dob = request.form['dob']
-        gender = request.form['gender']
-        phone = request.form['phone']
-        covid_status = request.form['covid-report']
-        blood_type = request.form['b-type']
-        #------------------
-        # password hashing
+        phoneNumber = request.form['phoneNumber']
         password = request.form['password']
-        cnf_password = request.form['cnf-password']
-        if password != cnf_password:
-            return "<span>Password Doesn't Match</span>"
-        
-        password = bytes(password,'utf-8')
-        password = hashlib.sha256(password).hexdigest()
-
-        # case 1: check if user does exists already
-        sql = "SELECT * FROM donors WHERE user_email =?"
-        stmt = ibm_db.prepare(conn, sql)
-        ibm_db.bind_param(stmt,1,email)
-        ibm_db.execute(stmt)
-        acc = ibm_db.fetch_assoc(stmt)
-        if acc:
-            return "<span> Account already Exists, Please login <a href='/login'>here</a></span>"
-
-        # case 2: validate the input if it matches the required pattern
-        if not re.match(r"^\S+@\S+\.\S+$", email):
-            return "<span> Please Enter Valid Email Address </span>"
-
-        insert_sql = "INSERT INTO  donors VALUES (?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?,?)"
-        prep_stmt = ibm_db.prepare(conn, insert_sql)
-        ibm_db.bind_param(prep_stmt, 1, first_name)
-        ibm_db.bind_param(prep_stmt, 2, last_name)
-        ibm_db.bind_param(prep_stmt, 3, email)
-        ibm_db.bind_param(prep_stmt, 4, addrss1)
-        ibm_db.bind_param(prep_stmt, 5, addrss2)
-        ibm_db.bind_param(prep_stmt, 6, state)
-        ibm_db.bind_param(prep_stmt, 7, pincode)
-        ibm_db.bind_param(prep_stmt, 8, dob)
-        ibm_db.bind_param(prep_stmt, 9, gender)
-        ibm_db.bind_param(prep_stmt, 10, phone)
-        ibm_db.bind_param(prep_stmt, 11, covid_status)
-        ibm_db.bind_param(prep_stmt, 12, blood_type)
-        ibm_db.bind_param(prep_stmt, 13, password)
-        ibm_db.execute(prep_stmt)
-
-        message = Mail(
-        from_email='sanjaysiva555@gmail.com',
-        to_emails=email,
-        subject='Confirmation on Registration with Plasma-Donor-App',
-        html_content='''
-            <h1>Registration Successfull</h1><br>
-            <p> Thank you so much registering with us </p><br>
-            <p> You are now registered donor </p>        
-        ''')
+        message = Mail(from_email='nutriaux04@gmail.com',to_emails=email,subject="NutriAux - Registration",html_content='<b>NutriAux welocmes you</b><br/><p>Your account has been registered successfully</p>')
         try:
-            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-            response = sg.send(message)
+            sg=SendGridAPIClient('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')
+            response=sg.send(message)
             print(response.status_code)
             print(response.body)
             print(response.headers)
         except Exception as e:
-            print(e.message)
+            print(e)
+        return redirect(url_for('personaldetails'))
+    
+
+
+
+@app.route("/personaldetails")
+def personaldetails():
+    return render_template("personaldetails.html")
+
+@app.route("/addpersonaldetails",methods=["POST","GET"])
+def addpersonaldetails():
+    if request.method == 'POST' :
+        age=float(request.form.get('age'))
+        gender=request.form.get('Gender')
+        weight=float(request.form.get('weight'))
+        height=float(request.form.get('height'))
+        activity=request.form.get('activity')
+        print(age,gender,weight,height,activity)
+        if(gender == 'male'and activity == "1"):
+            totalCalories = 1.2 * (66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age))
+        elif(gender == 'male' and activity == "2"):
+            totalCalories = 1.375 * (66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age))
+        elif (gender == 'male' and activity == "3"):
+            totalCalories = 1.55 * (66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age))
+        elif(gender == 'male' and activity == "4"):
+            totalCalories = 1.725 * (66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age))
+        elif(gender == 'male' and activity == "5"): 
+            totalCalories = 1.9 * (66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age))
+        elif(gender == 'female' and activity == "1"):
+            totalCalories = 1.2 * (655 + (9.563 * weight) + (1.850 * height) - (4.676 * age))
+        elif(gender == 'female' and activity == "2"):
+            totalCalories = 1.375 * (655 + (9.563 * weight) + (1.850 * height) - (4.676 * age))
+        elif(gender == 'female' and activity == "3"):
+            totalCalories = 1.55 * (655 + (9.563 * weight) + (1.850 * height) - (4.676 * age))
+        elif(gender == 'female' and activity == "4"):
+            totalCalories = 1.725* (655 + (9.563 * weight) + (1.850 * height) - (4.676 * age))
+        else: 
+            totalCalories = 1.9 * (655 + (9.563 * weight) + (1.850 * height) - (4.676 * age))
+        print(int(totalCalories))
+        BMI = (weight / (height/100)**2 )
+        if BMI <= 18.5:  
+            BMI_message="underweight"  
+        elif BMI <= 24.9:  
+            BMI_message="healthy"  
+        elif BMI <= 29.9:  
+            BMI_message="overweight"
+        else:
+            BMI_message="obese"   
+        print(BMI)
     return redirect(url_for('login'))
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html",message="")
 
-@app.route('/do_login',methods=['GET','POST'])
-def do_login():
-    if request.method == 'POST':
-        user_email = request.form['user_email']
-        password = request.form['password']
-        # salt the password 
-        password = bytes(password,'utf-8')
-        password = hashlib.sha256(password).hexdigest()
-
-        #query the db
-        sql = "SELECT * FROM donors WHERE user_email =? AND pass_word=?"
-        stmt = ibm_db.prepare(conn,sql)
-        ibm_db.bind_param(stmt,1,user_email)
-        ibm_db.bind_param(stmt,2,password)
-        ibm_db.execute(stmt)
-        acc = ibm_db.fetch_assoc(stmt)
-        if acc:
-            session['login_status'] = True
-            session['user_id'] = user_email.split('@')[0]
-            return redirect(url_for('home'))
-        
-        # check if the acc exists 
-        sql = "SELECT * FROM donors WHERE user_email=?"
-        stmt = ibm_db.prepare(conn,sql)
-        ibm_db.bind_param(stmt,1,user_email)
-        ibm_db.execute(stmt)
-        res = ibm_db.fetch_assoc(stmt)
-        if res:
-            return "<span> Account Exists, Seems Like Password is incorrect </span>"
-        else:
-            return "<span>Don't you have an account ? try register with us <a href='/register'>HERE</a></span>"
+@app.route("/verify",methods=["POST","GET"])
+def verify():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    return render_template("login.html",message="")
 
 
-@app.route('/logout')
-def logout():
-    session['login_status'] = False
-    session.pop('user_id',None)
-
-    return redirect(url_for('index'))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+Footer
